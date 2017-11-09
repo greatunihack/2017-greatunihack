1f69f39af1a4c244f56e94a49e6aae63c3e7ec9f
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-11-09 20:52:49 +0000

    Add Firebase

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,148 +1,65 @@
-$(document).ready(() => {
-  const schedule = new Schedule()
+var database;
 
-  schedule.main()
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    databaseURL: "https://greatunihack-6f7cd.firebaseio.com",
+    projectId: "greatunihack-6f7cd",
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  var ref = database.ref('/');
+  ref.on('value', gotData, errData);
 })
 
-class Schedule {
+function gotData(data) {
+  data = data.val();
+  console.log(data);
+  var events = data.events;
+  var eventImage = "images/guh_honeycomb.png"
+  var beeImage = "images/guh_logo.png"
 
-  constructor() {
-    this._active = 0
-    this.timestamp = new Date()
-    this._serverTimestamp = new Date()
-    this.createSchedule()
+  
+  $("#saturday").html("");
+  $("#sunday").html("");
+  _.forEach([1, 2], function(day) {
+    var dayEvents = _.filter(events, function(event) {
+      return event.day == day;
+    });
+    dayEvents = _.sortBy(dayEvents, function(event) {
+      return event.order;
+    });
 
-  }
-  simulateUpdate() {
-    this._active = (this._active + 1) % 12;
-    this._serverTimestamp = new Date()
-  }
-  async talkToServer() {
-    if (this.timestamp == this._serverTimestamp) {
-      return {
-        new: false
+    var activeEvents = _.filter(dayEvents, function(event) {
+      return event.active;
+    });
+    var firstActiveOrder = activeEvents.length > 0 ? activeEvents[0].order : -1;
+
+    _.forEach(dayEvents, function(event, index) {
+      var imageSrc = event.active ? beeImage : eventImage
+  
+      var image='<img src="'+imageSrc+'"style="width:30px;	vertical-align: middle; " />'
+      if (!event.active) image = "";
+      var label = event.label;
+      var time = event.time;
+      if (event.active) {
+        label = "<b class='active'>" + label + "</b>"
+        time = "<b class='active'>" + time + "</b>"
+      } else if (event.order < firstActiveOrder) {
+        label = "<s>"+label+"</s>";
+        time = "<s>"+time+"</s>";
       }
-    }
-
-    this.timestamp = this._serverTimestamp
-
-    return Promise.resolve({
-      new: true,
-      events: [
-        { id: 0, label: 'Registration', time: "09:00" },
-        { id: 1, label: 'Breakfast', time: "09:30" },
-        { id: 2, label: 'Opening Ceremony', time: "10:30" },
-        { id: 3, label: 'Hacking Starts', time:"12:00" },
-        { id: 4, label: 'Team Forming Session', time: "12:00" },
-        { id: 5, label: 'Workshop: American Express', time: "13:00" },
-        { id: 6, label: 'Lunch', time: "14:00" },
-        { id: 7, label: 'Workshop: Web Applications UK', time: "15:00" },
-        { id: 8, label: 'Workshop: Ocado', time: "16:30" },
-        { id: 9, label: 'Workshop: Auto Trader UK', time: "18:00" },
-        { id: 10, label: 'Dinner', time: "18:30" },
-        { id: 11, label: 'Workshop: Goldman Sachs', time: "19:30" },
-      ],
-      active: this._active
-    })
-  }
-
-  async pingServer() {
-    const apiUrl = ``
-
-    const data = await this.talkToServer()
-
-    if (data.new)
-      await this.updateSchedule(data)
-  }
-
-  createSchedule() {
-    return
-    var container = document.getElementById('mynetwork');
-
-    // provide the data in the vis format
-    var visData = {
-      nodes: [],
-      edges: []
-    };
-    var options = {
-      nodes: {
-        font: {
-          size: 14,
-          color: "#ffffff"
-        },
-        size: 40,
-      },
-      interaction: {
-        dragView: false,
-        dragNodes: false,
-        multiselect: false,
-        zoomView: false
+      if (!event.active) {
+        label = "<span class='inactive'>" + label + "</span>"
+        time = "<span class='inactive'>" + time + "</span>"
       }
-    };
-    
 
-    this.network = new vis.Network(container, visData, options);
-  }
-  async updateSchedule(data) {
-    const eventImage = "images/guh_honeycomb.png"
-    const beeImage = "images/guh_logo.png"
-
-    $("#saturday").html("")
-    data.events.forEach((event, index) => {
-      const imageSrc = event.id == data.active ? beeImage : eventImage
-
-      const image=`<img src=${imageSrc} style="width:30px;	vertical-align: middle; " />`
-      $("#saturday").append(`<tr><td>${image}</td><td>${event.time}</td><td>${event.label}</td></tr>`)
+      $(day==1?"#saturday":"#sunday").append('<tr><td>'+image+'</td><td>'+time+'</td><td>'+label+'</td></tr>');
     })
+  })
+}
 
-    return
-
-    const nodeOptions = {
-      shape: 'image',
-      allowedToMoveX: false,
-      allowedToMoveY: false
-    }
-
-    const eventOptions = { image: eventImage, ...nodeOptions }
-    const activeEventOptions = { image: beeImage, ...nodeOptions }
-
-    const cols = 4;
-
-    const nodes = new vis.DataSet(
-      data.events.map((event, index) => {
-        const inheritOptions = (data.active === event.id ? activeEventOptions : eventOptions)
-        return {
-          id: index,
-          label: event.label,
-          ...inheritOptions,
-          x: Math.floor(index % cols) * 150,
-          y: Math.floor(index / cols) * 200,
-          fixed: true,
-        }
-      })
-    )
-
-    // create an array with edges
-    var edges = new vis.DataSet(
-      data.events.map((event, index) => {
-        if (index < data.events.length)
-          return {
-            from: index,
-            to: index + 1,
-            smooth: true,
-
-          }
-      })
-    );
-
-    this.network.setData({nodes, edges})
-
-  }
-  async main() {
-
-    await this.pingServer()
-
-    setInterval(this.pingServer.bind(this), 500)
-    setInterval(this.simulateUpdate.bind(this), 2000)
-  }
+function errData(err) {
+  console.log(err);
 }
