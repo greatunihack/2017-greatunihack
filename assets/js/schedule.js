a8f043d43aaf9bb3ff11c465305cdb0546e2aced
Alex Dumitru <alex.dumitru@student.manchester.ac.uk>
2017-11-27 01:53:08 +0000

    Implement Andrei Muntean's code cleanup suggestions

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -4,12 +4,12 @@ var data = {};
 $(document).ready(function () {
   // Initialize Firebase
   var config = {
-    databaseURL: "https://greatunihack-6f7cd.firebaseio.com",
-    projectId: "greatunihack-6f7cd",
+    databaseURL: 'https://greatunihack-6f7cd.firebaseio.com',
+    projectId: 'greatunihack-6f7cd',
   };
 
   firebase.initializeApp(config);
-  var database = firebase.database();
+  database = firebase.database();
   var ref = database.ref('/');
   ref.on('value', gotData, errData);
 
@@ -19,7 +19,7 @@ $(document).ready(function () {
 function getToday() {
   // always check for GMT+0.
   var timezone = 'Europe/London';
-  
+
   var now = moment().tz(timezone);
   var time = now.format('HH:mm');
   var date = now.format('MMM D YYYY');
@@ -27,8 +27,9 @@ function getToday() {
   return {
     time: time,
     date: date
-  }
+  };
 }
+
 function gotData(newData) {
   data = newData.val();
   updateSchedule();
@@ -36,21 +37,21 @@ function gotData(newData) {
 
 function updateSchedule() {
   var events = data.events;
-  var eventImage = "images/guh_honeycomb.png"
-  var beeImage = "images/guh_logo.png"
+  var eventImage = 'images/guh_honeycomb.png'
+  var beeImage = 'images/guh_logo.png'
 
   var todayDate = getToday();
   var now = todayDate.time;
   var today = new Date(todayDate.date);
-  
+
   var dates = {
-    saturday: new Date("Nov 11 2017"),
-    sunday: new Date("Nov 12 2017")
+    saturday: new Date('Nov 11 2017'),
+    sunday: new Date('Nov 12 2017')
   };
 
-  $("#saturday").html("");
-  $("#sunday").html("");
-  _.forEach(["saturday", "sunday"], function (day) {
+  $('#saturday').html('');
+  $('#sunday').html('');
+  _.forEach(['saturday', 'sunday'], function (day) {
     var dayEvents = events[day];
     dayEvents = _.sortBy(dayEvents, function (event) {
       return event.time;
@@ -59,7 +60,7 @@ function updateSchedule() {
     var activeEvents = _.filter(dayEvents, function (event, index) {
       var active = today.toDateString() === dates[day].toDateString();
       active = active && now >= event.time;
-      if (index + 1 < dayEvents.length) { // do we have other events after this? 
+      if (index + 1 < dayEvents.length) { // do we have other events after this?
         var nextIndex = index + 1;
         while (dayEvents[nextIndex].time == event.time) { // let's find the first event that doesn't happen at this time!
           nextIndex++;
@@ -73,28 +74,28 @@ function updateSchedule() {
       event.active = active;
       return event.active;
     });
-    var firstActiveTime = activeEvents.length > 0 ? activeEvents[0].time : "00:00";
+    var firstActiveTime = activeEvents.length > 0 ? activeEvents[0].time : '00:00';
 
     _.forEach(dayEvents, function (event, index) {
       var imageSrc = event.active ? beeImage : eventImage
 
       var image = '<img src="' + imageSrc + '"style="width:30px;	vertical-align: middle; " />'
-      if (!event.active) image = "";
+      if (!event.active) image = '';
       var label = event.label;
       var time = event.time;
       if (event.active) {
-        label = "<b class='active'>" + label + "</b>"
-        time = "<b class='active'>" + time + "</b>"
+        label = '<b class="active">' + label + '</b>'
+        time = '<b class="active">' + time + '</b>'
       } else if (event.time < firstActiveTime) {
-        label = "<s>" + label + "</s>";
-        time = "<s>" + time + "</s>";
+        label = '<s>' + label + '</s>';
+        time = '<s>' + time + '</s>';
       }
       if (!event.active) {
-        label = "<span class='inactive'>" + label + "</span>"
-        time = "<span class='inactive'>" + time + "</span>"
+        label = '<span class="inactive">' + label + '</span>'
+        time = '<span class="inactive">' + time + '</span>'
       }
 
-      $("#" + day).append('<tr><td>' + image + '</td><td>' + time + '</td><td>' + label + '</td></tr>');
+      $('#' + day).append('<tr><td>' + image + '</td><td>' + time + '</td><td>' + label + '</td></tr>');
     })
   })
 }
